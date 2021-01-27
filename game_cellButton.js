012b4634f3432bf3d1f89977f9165d3985b6a1f7
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 17:31:12 -0500

    Create game_cellButton.js

---
+++ game_cellButton.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class CellButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+      let verb = this.props.verb;
+      let newMessage = "";
+      if (verb === "push" && !this.props.interactStates.buttonPressed) {
+        newMessage = "You press the button and hear a heavy 'CLICK' from behind you";
+        this.props.interactions.buttonPress();
+      } else if (verb === "look") {
+        newMessage = "It definitely appears to be a button of some sort.";
+      } else if (verb === "push") {
+        newMessage = "It's already been pressed in.  It does nothing now.";
+      } else if (verb === "pickUp") {
+          newMessage = "You can't pick that up.";
+      }
+      this.props.changeMessage(newMessage);
+  }
+  
+  render() {
+      return(
+            <button onClick={this.handleClick}>
+                Button
+            </button>
+    )
+  }
+}
