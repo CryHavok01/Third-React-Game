e1daff98b278be203dccbbcf97721bee3083245c
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 17:35:36 -0500

    Create game_win.js

---
+++ game_win.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class Win extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+    handleClick() {
+        this.props.reset();
+    }
+  render() {
+    return(
+        <div>
+            You've escaped to freedom!  Congratulations, I hope you learned something from this experience.  I dunno, like, don't get locked in a dungeon?  Or something?  Cool.  Wanna play again? <button onClick={this.handleClick}>Start Over</button>
+        </div>
+    )
+  }
+}
