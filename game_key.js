7ef3ff31ed698a07b615a0d2d6c7cf408b0b3da6
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 17:34:23 -0500

    Create game_key.js

---
+++ game_key.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Key extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+      let verb = this.props.verb;
+      let newMessage;
+      if (verb === "push") {
+        newMessage = "Why do you want to do that?";    
+      } else if (verb === "look") {
+        newMessage = "It's a key that can fit in a lock.";
+      } else if (verb === "pickUp") {
+          newMessage = "You grab the key and take it with you";
+          let newItem = "Key";
+          this.props.interactions.pickUp(newItem);
+      }
+      this.props.changeMessage(newMessage);
+  }
+  
+  render() {
+      return(
+        <div>
+            <button onClick={this.handleClick}>
+                Key
+            </button>
+        </div>
+    )
+  }
+}
