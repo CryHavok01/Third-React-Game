65e2c4ba48819d934cc34bfeceb574dacd3c5a66
Josh Halpern <jhhalpern@gmail.com>
2021-02-01 17:15:51 -0500

    Create game_room4.js

---
+++ game_room4.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Room4 extends React.Component {
+  render() {
+    return(
+      <div>
+        You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces.  Against the far railing is a large wooden crate.  The only exit is the stairs leading back down.
+      </div>
+    )
+  }
+}
