932b3e055da64c58459226f6dae0393b999cc935
Josh Halpern <jhhalpern@gmail.com>
2021-02-01 17:14:49 -0500

    Update game_room3.js

--- game_room3.js
+++ game_room3.js
@@ -1,13 +1,11 @@
 'use strict';
 
 class Room3 extends React.Component {
-
-    render() {
-      return(
-        <div>
-          You emerge into a parking garage.  There are a few parked cars scattered around.  Next to you is a closed metal box attached to the wall.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
-        </div>
-            )
-        }
-      }
+  render() {
+    return(
+      <div>
+        You emerge into a parking garage.  There are a few parked cars scattered around.  Next to you is a closed metal box attached to the wall.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
+      </div>
+    )
   }
+}
