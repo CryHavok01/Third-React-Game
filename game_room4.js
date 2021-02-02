4dadd1731e20d36b491761f140f256d744ac6b0a
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 17:37:34 -0500

    Update game_room4.js

--- game_room4.js
+++ game_room4.js
@@ -5,7 +5,13 @@ class Room4 extends React.Component {
     if (this.props.interactStates.carDoorOpen && this.props.inventory.includes("ID Card")) {
       return(
         <div>
-          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces with its door wide open.  Against the far railing is an open tool crate.  The only exit is the <Stairs 
+          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces with its door wide open.  Against the far railing is an open tool <Crate 
+            verb={this.props.verb}
+            inventory={this.props.inventory}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions}
+            interactStates={this.props.interactStates}
+          />.  The only exit is the <Stairs 
             room={this.props.room}
             changeMessage={this.props.changeMessage}
             changeRoom={this.props.changeRoom} 
@@ -15,7 +21,13 @@ class Room4 extends React.Component {
     } else if (this.props.interactStates.carDoorOpen) {
       return(
         <div>
-          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces with its door wide open.  On the dashboard of the car is an ID Card.  Against the far railing is an open tool crate.  The only exit is the <Stairs 
+          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces with its door wide open.  On the dashboard of the car is an ID Card.  Against the far railing is an open tool <Crate 
+            verb={this.props.verb}
+            inventory={this.props.inventory}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions}
+            interactStates={this.props.interactStates}
+          />.  The only exit is the <Stairs 
             room={this.props.room}
             changeMessage={this.props.changeMessage}
             changeRoom={this.props.changeRoom} 
@@ -25,7 +37,13 @@ class Room4 extends React.Component {
     } else if {this.props.interactStates.toolCrateOpen && this.props.inventory.includes("Crowbar")) {
       return(
         <div>
-          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces.  Against the far railing is an open tool crate.  The only exit is the <Stairs 
+          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces.  Against the far railing is an open tool <Crate 
+            verb={this.props.verb}
+            inventory={this.props.inventory}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions}
+            interactStates={this.props.interactStates}
+          />.  The only exit is the <Stairs 
             room={this.props.room}
             changeMessage={this.props.changeMessage}
             changeRoom={this.props.changeRoom} 
@@ -35,7 +53,13 @@ class Room4 extends React.Component {
     } else if {this.props.interactStates.toolCrateOpen) {
       return(
         <div>
-          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces.  Against the far railing is an open tool crate.  Inside are a variety of tools including a crowbar.  The only exit is the <Stairs 
+          You are on the roof of the parking garage.  There is a cop car parked diagonally across three spaces.  Against the far railing is an open tool <Crate 
+            verb={this.props.verb}
+            inventory={this.props.inventory}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions}
+            interactStates={this.props.interactStates}
+          />.  Inside are a variety of tools including a crowbar.  The only exit is the <Stairs 
             room={this.props.room}
             changeMessage={this.props.changeMessage}
             changeRoom={this.props.changeRoom} 
