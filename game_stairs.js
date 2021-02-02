c0ed243a4a559cf43cb5747381fb6060dd6fa926
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 14:38:07 -0500

    Create game_stairs.js

---
+++ game_stairs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Stairs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+      let newMessage;
+      let newRoom;
+      if (this.props.room === "room3") {
+            let newRoom = "room4";
+            let newMessage = "You climb the up stairs";
+            this.props.changeRoom(newRoom);
+            this.props.changeMessage(newMessage);
+      } else if (this.props.room === "room4") {
+            let newRoom = "room3";
+            let newMessage = "You descend the stairs";
+            this.props.changeRoom(newRoom);
+            this.props.changeMessage(newMessage);
+      }
+  }
+  
+  render() {
+      return(
+            <button onClick={this.handleClick}>
+                Stair Case
+            </button>
+      )
+  }
+}
