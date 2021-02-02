0821f847338e8a7ae95af769b7f054d19c6e5d9c
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 15:12:46 -0500

    Create game_toolCrate.js

---
+++ game_toolCrate.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Crate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+      let verb = this.props.verb;
+      let newMessage = "";
+      if (verb === "push") {
+        newMessage = "The crate is too heavy to move, but it doesn't appear to be locked";    
+      } else if (verb === "look") {
+        newMessage = "It's a big wooden crate with a heavy lid, but you don't see any locks";
+      } else if (verb === "pickUp") {
+          newMessage = "You can't pick that up";
+      } else if (verb === "use") {
+          newMessage = "With some effort, you swing the lid open.  Inside the crate are a few shovels, a bag of rock salt, various tools, and one crow bar.";
+      }
+      this.props.changeMessage(newMessage);
+  }
+  
+  render() {
+      return(
+        <div>
+            <button onClick={this.handleClick}>
+            Crate
+            </button>
+        </div>
+    )
+  }
+}
