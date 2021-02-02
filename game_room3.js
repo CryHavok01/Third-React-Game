583cd98b522b2297ef73b34dd557b49407d1038d
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 17:16:54 -0500

    Update game_room3.js

--- game_room3.js
+++ game_room3.js
@@ -5,19 +5,49 @@ class Room3 extends React.Component {
     if (this.props.interactStates.garageGateOpen) {
       return(
         <div>
-          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  The gate blocking the exit has lifted.  In the corner is a stair case leading up.
+          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  The gate blocking the exit has lifted.  In the corner is a <Stairs 
+            room={this.props.room}
+            changeMessage={this.props.changeMessage}
+            changeRoom={this.props.changeRoom} 
+          /> leading up.
         </div>
       )
     } else if (this.props.interactStates.keyBoxOpen && this.props.inventory.includes("Car Key")) {
       return(
         <div>
-          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
+          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  There is a metal <Gate 
+            verb={this.props.verb} 
+            changeMessage={this.props.changeMessage}
+          /> blocking the exit.  Next to the gate is a <CardReader 
+            verb={this.props.verb} 
+            item={this.props.item}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions} 
+            interactStates={this.props.interactStates}
+          />.  In the corner is a <Stairs 
+            room={this.props.room}
+            changeMessage={this.props.changeMessage}
+            changeRoom={this.props.changeRoom} 
+          /> leading up.
         </div>
       )
     } else if (this.props.interactStates.keyBoxOpen) {
       return(
         <div>
-          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is a metal box with a Hello Kitty keychain inside.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
+          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is a metal box with a Hello Kitty keychain inside.  There is a metal <Gate 
+            verb={this.props.verb} 
+            changeMessage={this.props.changeMessage}
+          /> blocking the exit.  Next to the gate is a <CardReader 
+            verb={this.props.verb} 
+            item={this.props.item}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions} 
+            interactStates={this.props.interactStates}
+          />.  In the corner is a <Stairs 
+            room={this.props.room}
+            changeMessage={this.props.changeMessage}
+            changeRoom={this.props.changeRoom} 
+          /> leading up.
         </div>
       )
     } else {
@@ -39,6 +69,7 @@ class Room3 extends React.Component {
             interactions={this.props.interactions} 
             interactStates={this.props.interactStates}
           />.  In the corner is a <Stairs 
+            room={this.props.room}
             changeMessage={this.props.changeMessage}
             changeRoom={this.props.changeRoom} 
           /> leading up.
