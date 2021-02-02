e6dbab8485b5c2fc40d440860081313eec8b97e1
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 17:07:34 -0500

    Update game_room3.js

--- game_room3.js
+++ game_room3.js
@@ -5,7 +5,13 @@ class Room3 extends React.Component {
     if (this.props.interactStates.garageGateOpen) {
       return(
         <div>
-          You are in a parking garage.  There are a few parked cars scattered around.  The gate blocking the exit has lifted.  In the corner is a stair case leading up.
+          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  The gate blocking the exit has lifted.  In the corner is a stair case leading up.
+        </div>
+      )
+    } else if (this.props.interactStates.keyBoxOpen && this.props.inventory.includes("Car Key")) {
+      return(
+        <div>
+          You are in a parking garage.  There are a few parked cars scattered around.  Attached to the wall is an empty metal box.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
         </div>
       )
     } else if (this.props.interactStates.keyBoxOpen) {
@@ -17,7 +23,25 @@ class Room3 extends React.Component {
     } else {
       return(
         <div>
-          You emerge into a parking garage.  There are a few parked cars scattered around.  Next to you is a closed metal box attached to the wall.  There is a metal gate blocking the exit.  Next to the gate is a card reader.  In the corner is a stair case leading up.
+          You emerge into a parking garage.  There are a few parked cars scattered around.  Next to you is a closed metal <Box 
+            verb={this.props.verb} 
+            item={this.props.item}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions} 
+            interactStates={this.props.interactStates}
+          /> attached to the wall.  There is a metal <Gate 
+            verb={this.props.verb} 
+            changeMessage={this.props.changeMessage}
+          /> blocking the exit.  Next to the gate is a <CardReader 
+            verb={this.props.verb} 
+            item={this.props.item}
+            changeMessage={this.props.changeMessage}
+            interactions={this.props.interactions} 
+            interactStates={this.props.interactStates}
+          />.  In the corner is a <Stairs 
+            changeMessage={this.props.changeMessage}
+            changeRoom={this.props.changeRoom} 
+          /> leading up.
         </div>
       )
     }
