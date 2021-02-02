210d1cca83e78555e6f22e3e57781de531e7aa17
Josh Halpern <jhhalpern@gmail.com>
2021-02-02 17:00:38 -0500

    Update game_window.js

--- game_window.js
+++ game_window.js
@@ -35,6 +35,36 @@ class Window extends React.Component {
                 />
             </div>
         )
+    } else if (this.props.room === "room3") {
+        return(
+            <div className="window">
+                <Room3
+                    verb={this.props.verb} 
+                    item={this.props.item}
+                    room={this.props.room}
+                    inventory={this.props.inventory}
+                    changeMessage={this.props.changeMessage} 
+                    changeRoom={this.props.changeRoom}
+                    interactions={this.props.interactions}
+                    interactStates={this.props.interactStates}
+                />
+            </div>
+        )
+    } else if (this.props.room === "room4") {
+        return(
+            <div className="window">
+                <Room4
+                    verb={this.props.verb} 
+                    item={this.props.item}
+                    room={this.props.room}
+                    inventory={this.props.inventory}
+                    changeMessage={this.props.changeMessage} 
+                    changeRoom={this.props.changeRoom}
+                    interactions={this.props.interactions}
+                    interactStates={this.props.interactStates}
+                />
+            </div>
+        )
     } else if (this.props.room === "win") {
         return(
             <div className="window">
