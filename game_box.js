c5bd5ac6a9007708640f8e0d4cdf04066f00a03c
Josh Halpern <jhhalpern@gmail.com>
2021-02-03 15:26:37 -0500

    Update game_box.js

--- game_box.js
+++ game_box.js
@@ -9,14 +9,42 @@ class Box extends React.Component {
   handleClick() {
       let verb = this.props.verb;
       let newMessage = "";
-      if (verb === "push") {
-        newMessage = "It won't move";    
-      } else if (verb === "look") {
-        newMessage = "It's a metal box a few inches thick.  It's firmly latched shut.";
-      } else if (verb === "pickUp") {
+      let item = this.props.item;
+      let droppedItem = "Crowbar";
+      if (this.props.inventory.includes("Car Key")) {
+        if (verb === "push") {
+          newMessage = "It won't move";    
+        } else if (verb === "look") {
+          newMessage = "It's a metal box with several hooks to hang keys from.  It is currently empty.";
+        } else if (verb === "pickUp") {
+            newMessage = "You can't pick that up";
+        } else if (verb === "use") {
+            newMessage = "It's already open";
+        }
+      } else if (this.props.interactStates.keyBoxOpen) {
+        if (verb === "push") {
+          newMessage = "It won't move";    
+        } else if (verb === "look") {
+          newMessage = "It's a metal box with several hooks to hang keys from.  There is a Hello Kitty keychain hanging from one of the hooks.";
+        } else if (verb === "pickUp") {
+            newMessage = "You can't pick that up";
+        } else if (verb === "use") {
+            newMessage = "It's already open";
+        }
+      } else {
+        if (verb === "push") {
+          newMessage = "It won't move";    
+        } else if (verb === "look") {
+          newMessage = "It's a metal box a few inches thick.  It's firmly latched shut.";
+        } else if (verb === "pickUp") {
           newMessage = "You can't pick that up";
-      } else if (verb === "use") {
+        } else if (verb === "use" && item === "Crowbar") {
+          newMessage = "You jam the crowbar under the edge of the lid and push.  With a snap, the lid flies open revealing several small hooks arranged in rows.  All of the hook are emtpy, except for one which has a Hello Kitty keychain hanging from it."
+          this.interactions.keyBoxOpen();
+          this.interactions.dropItem(droppedItem);
+        } else if (verb === "use") {
           newMessage = "You can't pry it open with your hands";
+        }
       }
       this.props.changeMessage(newMessage);
   }
@@ -25,7 +53,7 @@ class Box extends React.Component {
       return(
         <div>
             <button onClick={this.handleClick}>
-            Box
+              Box
             </button>
         </div>
     )
